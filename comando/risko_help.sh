1507062cc7ea63d07e5ee0c429c5f210254e3a81
InfraestructuraRisko <69139245+InfraestructuraRisko@users.noreply.github.com>
2020-08-31 02:09:56 -0600

    Update risko_help.sh

--- comando/risko_help.sh
+++ comando/risko_help.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
+  #INFERFACES
 MOD_INTERFACES='sudo nano /etc/network/interfaces'
-echo "VARIABLES ACTUALIZADAS CORRECTAMENTE"
+SAVE_INTERFACES='/etc/init.d/networking restart'
+  #SAMBA
+$CONF_SAMBA='sudo nano /etc/samba/smb.conf'
+$RESTART_SAMBA='service smbd restart'
+$STATUSUS_SAMBA='service smbd status'
+  #DHCP
+$CONF_DHCP='sudo nano /etc/dhcp/dhcpd.conf'
+$RESTART_DHCP='service isc-dhcp-server restart'
+$STATUS_DHCP='service isc-dhcp-server status'
+  #DNS
+$CONF_DNS='sudo nano /etc/bind/db.riskodns.com'
+$RESTART_DNS='service bind9 restart'
+$STATUS_DNS='service bind9 status'
+ #PROXY
+$CONF_PROXY='sudo nano /etc/squid/squid.conf'
+$RESTART_PROXY='service squid restart'
+$STATUS_PROXY='service squid status'
+ #VPN
+$CONF_VPN='sudo nano /etc/squid/squid.conf'
+$RESTART_VPN='service squid restart'
+$RESTART_VPN='service squid status'
+echo "VARIABLES ACTUALIZADAS"
 
 
