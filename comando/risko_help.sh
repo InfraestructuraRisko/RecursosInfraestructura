56ef789721a4ff4370bd912b504694955efbaceb
InfraestructuraRisko <69139245+InfraestructuraRisko@users.noreply.github.com>
2020-08-31 02:47:21 -0600

    Update risko_help.sh

--- comando/risko_help.sh
+++ comando/risko_help.sh
@@ -1,27 +1,27 @@
-#!/bin/sh
+
   #INFERFACES
-export MOD_INTERFACES="sudo nano /etc/network/interfaces"
-export SAVE_INTERFACES='/etc/init.d/networking restart'
+sudo nano /etc/network/interfaces
+/etc/init.d/networking restart
   #SAMBA
-export CONF_SAMBA='sudo nano /etc/samba/smb.conf'
-export RESTART_SAMBA='service smbd restart'
-export STATUS_SAMBA=service smbd status
+sudo nano /etc/samba/smb.conf
+service smbd restart
+service smbd status
   #DHCP
-export CONF_DHCP='sudo nano /etc/dhcp/dhcpd.conf'
-export RESTART_DHCP='service isc-dhcp-server restart'
-export STATUS_DHCP="service isc-dhcp-server status"
+sudo nano /etc/dhcp/dhcpd.conf
+service isc-dhcp-server restart
+service isc-dhcp-server status
   #DNS
-export CONF_DNS='sudo nano /etc/bind/db.riskodns.com'
-export RESTART_DNS='service bind9 restart'
-export STATUS_DNS='service bind9 status'
- #PROXY
-export CONF_PROXY='sudo nano /etc/squid/squid.conf'
-export RESTART_PROXY='service squid restart'
-export STATUS_PROXY='service squid status'
+sudo nano /etc/bind/db.riskodns.com
+service bind9 restart
+Service bind9 status
+ #PROXs
+sudo nano /etc/squid/squid.conf
+service squid restart
+service squid status
  #VPN
-export CONF_VPN='sudo nano /etc/pptpd/pptpd.conf'
-export RESTART_VPN='service pptpd restart'
-export STATUS_VPN='service pptpd status'
-echo "VARIABLES ACTUALIZADAS"
+sudo nano /etc/pptpd/pptpd.conf
+service pptpd restart
+service pptpd status
+
 
 
